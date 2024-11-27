70c77d8e230aadc2d9b3d4521bda825f6ce73e3e
shrinidhivelan <shrinidhi.velan@epfl.ch>
2024-11-27 19:48:54 +0100

    ran for all the mice

--- main.py
+++ main.py
@@ -11,18 +11,23 @@ from helpers import *
 def main():
 
     #### Replace the following by wherever your base data is:
-    folder_path = '/Volumes/LaCie/EPFL/Master sem3/Semester Project Lsens/Mice_data/context'
-    save_path = '/Volumes/LaCie/EPFL/Master sem3/Semester Project Lsens/Data'
+
+    ####### DON't forget to change context_fill_in into context simply :!!!!!
+    #folder_path = '/Volumes/LaCie/EPFL/Master sem3/Semester Project Lsens/Mice_data/context'
+    folder_path = '/Volumes/LaCie/EPFL/Mastersem3/Semester Project Lsens/Mice_data/context_fill_in'
+    save_path = '/Volumes/LaCie/EPFL/Mastersem3/Semester Project Lsens/Data'
 
     print("Starting the analysis...")
     
     #### (1) Generate 1 mouse name and create the selectivity parquet file: ####
     #mouse_names = generate_mice_data(folder_path, save_path)
 
+    """ 
     ### Un comment the following if you do not want to regenerate the whole data : 
     mouse_names = ['AB124_20240815_111810','AB125_20240817_123403','AB126_20240822_114405','AB130_20240902_123634','AB129_20240828_112850','AB128_20240829_112813','AB127_20240821_103757']
     #mouse_names = ['AB126_20240822_114405', 'AB127_20240821_103757', 'AB128_20240829_112813', 'AB129_20240828_112850', 'AB130_20240902_123634']
-    mouse_names = ['AB124_20240815_111810','AB125_20240817_123403','AB126_20240822_114405','AB130_20240902_123634','AB129_20240828_112850','AB128_20240829_112813','AB127_20240821_103757',
+    """
+    mouse_names = ['AB120_20240811_143102','AB121_20240813_125401','AB124_20240815_111810','AB125_20240817_123403','AB126_20240822_114405','AB130_20240902_123634','AB129_20240828_112850','AB128_20240829_112813','AB127_20240821_103757',
                'AB123_20240806_110231', 'AB122_20240804_134554', 'AB119_20240731_102619', 'AB117_20240723_125437', 'AB116_20240724_102941']
 
     """ 
@@ -39,11 +44,11 @@ def main():
     #### (2a) Generate AUC plots :
     for mouse_name in mouse_names:
         process_and_save_roc(mouse_name, save_path)
-    
     """
+    
     #convert_to_csv(mouse_names)
 
-    #put_together(save_path, mouse_names)
+    put_together(save_path, mouse_names)
 
 
 
